ffc7eb270a745785db1cce0626a09e104a913520
kenoba10 <thekenoba10@gmail.com>
2015-02-07 11:14:27 -0500

    Official 1.0.0 Release

---
+++ obj/x86/Debug/STBEngine.res.shaders.guiVS.glsl
@@ -0,0 +1,24 @@
+﻿#version 330 core
+
+layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 textureCoordinates;
+
+out Vertex
+{
+	
+	vec2 position;
+	vec2 textureCoordinates;
+
+} vertex;
+
+uniform mat4 projection;
+
+void main()
+{
+	
+	gl_Position = projection * vec4(position, 1, 1);
+	
+	vertex.position = position;
+	vertex.textureCoordinates = textureCoordinates;
+	
+}
---
+++ obj/x86/Release/STBEngine.res.shaders.guiFS.glsl
@@ -0,0 +1,26 @@
+﻿#version 330 core
+
+layout (location = 0) out vec4 color;
+
+in Vertex
+{
+	
+	vec2 position;
+	vec2 textureCoordinates;
+
+} vertex;
+
+uniform int useTexture;
+
+uniform vec4 baseColor;
+uniform sampler2D activeTexture;
+
+void main()
+{
+	
+	color = baseColor;
+	
+	if(useTexture == 1)
+		color *= texture(activeTexture, vertex.textureCoordinates);
+	
+}
